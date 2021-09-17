3ab293fca9a4399d61eb60b19261734b26b85146
octave collombel <octavec@outlook.com>
2021-09-17 23:43:21 +0200

    check not passing

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(response):
     """ For Teardown """
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
