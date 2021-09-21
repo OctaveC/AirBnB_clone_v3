587d6a4c31316e78a14ccff344f274c816f1298d
octave collombel <octavec@outlook.com>
2021-09-21 21:48:41 +0200

    exo 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,7 @@ from models.state import State
 
 @app_views.route('/states/<string:state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-def get_cities(state_id):
+def all_cities(state_id):
     """ Retrieves the list of all cities. """
     state = storage.get(State, state_id)
     if state is None:
@@ -22,7 +22,7 @@ def get_cities(state_id):
 
 @app_views.route('/cities/<string:city_id>', methods=['GET'],
                  strict_slashes=False)
-def get_city(city_id):
+def one_city(city_id):
     """ Retrieves a single city object. """
     city = storage.get(City, city_id)
     if city is None:
@@ -32,7 +32,7 @@ def get_city(city_id):
 
 @app_views.route('/cities/<string:city_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_city(state_id):
+def delete_city(city_id):
     """ Deletes a city object. """
     city = storage.get(City, city_id)
     if city is None:
@@ -44,15 +44,15 @@ def delete_city(state_id):
 
 @app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
                  strict_slashes=False)
-def create_city():
+def create_city(state_id):
     """ Creates a new city object. """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in request.get_json():
-        return jsonify({'error': 'Missing name'}), 400
+        return make_response(jsonify({'error': 'Missing name'}), 400)
     kwargs = request.get_json()
     kwargs['state_id'] = state_id
     city = City(**kwargs)
@@ -62,7 +62,7 @@ def create_city():
 
 @app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
-def update_city(state_id):
+def update_city(city_id):
     """ Updates a city object. """
     city = storage.get(City, city_id)
     if city is None:
