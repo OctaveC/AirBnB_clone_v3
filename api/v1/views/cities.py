061d7b3c9271980aa5206a8fb88e5c85a1c13f0b
octave collombel <octavec@outlook.com>
2021-09-21 21:23:33 +0200

    exo 8

---
+++ api/v1/views/cities.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" visual cities.py """
+from flask import jsonify, abort, make_response, request
+from api.v1.views import app_views
+from models.state import State
+from models.state import City
+from models import storage
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def all_city():
+    """ Retrieves the list of all cities. """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
+
+
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def one_city(state_id):
+    """ Retrieves a single city object. """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(state_id):
+    """ Deletes a city object. """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    city.delete()
+    storage.save()
+    return (jsonify({}))
+
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def create_city():
+    """ Creates a new city object. """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' not in request.get_json():
+        return jsonify({'error': 'Missing name'}), 400
+    kwargs = request.get_json()
+    kwargs['state_id'] = state_id
+    city = City(**kwargs)
+    city.save()
+    return make_response(jsonify(city.to_dict()), 201)
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_city(state_id):
+    """ Updates a city object. """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city, attr, val)
+    city.save()
+    return jsonify(city.to_dict())
